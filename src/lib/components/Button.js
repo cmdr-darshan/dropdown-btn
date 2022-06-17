fe54450c6496e24ee8e100504dd28133e2739e4f
cmdr-darshan <darshan@initcoders.com>
2022-06-17 11:37:28 +0530

    Initial Commit

--- src/lib/components/Button.js
+++ src/lib/components/Button.js
@@ -6,6 +6,7 @@ const Button = (props) => {
       <button className={`btn btn--${props.kind} CTA`} data-id={props.id} type={props.type} name={props.name} value={props.value} disabled={props.disabled} onClick={props.onClick}> 
            <h4>{props.label}</h4>
       </button>
+      
    )
 }
 export default Button;
